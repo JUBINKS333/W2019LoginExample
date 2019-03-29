725ae81994d1e2604d4a98dbb8665919a4900e54
moxDroid Labs Inc <pritam.world@gmail.com>
2019-03-28 20:09:11 -0400

    Login design and code updated

--- app/src/main/java/com/pritesh/w2019loginexample/MainActivity.java
+++ app/src/main/java/com/pritesh/w2019loginexample/MainActivity.java
@@ -1,5 +1,6 @@
 package com.pritesh.w2019loginexample;
 
+import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -14,7 +15,6 @@ public class MainActivity extends AppCompatActivity
     private EditText edtUserEmail;
     private EditText edtPassword;
     private Button btnLogin;
-    private Button btnClear;
     private TextView txtMsg;
 
     @Override
@@ -29,7 +29,6 @@ public class MainActivity extends AppCompatActivity
         txtMsg = findViewById(R.id.lblMessage);
 
         btnLogin = findViewById(R.id.btnSignIn);
-        btnClear = findViewById(R.id.btnClear);
 
         btnLogin.setOnClickListener(new View.OnClickListener()
         {
@@ -37,8 +36,20 @@ public class MainActivity extends AppCompatActivity
             public void onClick(View view)
             {
                 String email = edtUserEmail.getText().toString();
+                String pwd = edtPassword.getText().toString();
 
-                Toast.makeText(MainActivity.this, email, Toast.LENGTH_SHORT).show();
+                if(email.equals("admin") && pwd.equals("admin@123"))
+                {
+                    txtMsg.setText("Login Success");
+                    txtMsg.setTextColor(Color.GREEN);
+                }
+                else
+                {
+                    txtMsg.setText("Email/Password invalid");
+                    txtMsg.setTextColor(Color.RED);
+                }
+
+                //Toast.makeText(MainActivity.this, email, Toast.LENGTH_SHORT).show();
             }
         });
 
